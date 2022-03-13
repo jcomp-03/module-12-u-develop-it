1833dcf81511982ef9ec7349b5c94b28684331d0
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-03-13 14:08:44 -0400

    Dropped database and created new connection with seeded table

--- __tests__/inputCheck.test.js
+++ __tests__/inputCheck.test.js
@@ -1,11 +1,15 @@
 const inputCheck = require('../utils/inputCheck');
 
+// Test #1
+// the function inputCheck() will return null if
+// all the properties are defined and not an empty string
 test('inputCheck() returns null when all properties exist', () => {
   const obj = {name: 'alice'};
 
   expect(inputCheck(obj, 'name')).toBe(null);
 });
 
+// Test #2
 test('inputCheck() returns an object when a property is missing', () => {
   const obj = {name: 'alice', occupation: ''};
 
