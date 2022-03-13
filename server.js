349a159743b6cddb93fcb55204483845b3c1fc30
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-03-13 14:36:00 -0400

    Created mysql queries for read, delete, and create operations

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,13 +8,55 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-/* app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World',
-      secondMessage: "Again, hello!"
-    });
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Momentarily commented out
+/* db.query(`SELECT * FROM candidates`, (err, rows) => {
+    if(err) {
+        console.log(err.code, err.message);
+    } else {
+        console.log(rows);
+    }
 }); */
 
+// GET a single candidate
+/* db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+}); */
+
+// Delete a candidate
+/* db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+}); */
+
+// Create a candidate
+/* const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+}); */
 
 
 // Default response for any other request (Not Found)
