7b0ffd4b7db1ec00bd5cb32ecb7ef80ebdf82678
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-03-17 15:36:57 -0400

    Created voters table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@
 -- before dropping parties table
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -21,4 +22,12 @@ CREATE TABLE candidates (
   -- it doesn't also exist in the parties table. If candidate's party is
   -- is deleted in the parties table, set the party_id field to NULL
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
